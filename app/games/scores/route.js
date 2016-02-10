af219cf9e62fb784ca68c89f6cfbfd3417fabd0a
Ryan Tablada <ryan.tablada@gmail.com>
2016-02-09 18:27:46 -0600

    Show details for a game

--- app/games/scores/route.js
+++ app/games/scores/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
+const {all, hash} = Ember.RSVP;
 
 export default Ember.Route.extend({
+  model(params) {
+    return fetch(`http://localhost:8000/games/${params.gameId}`)
+      .then((res) => res.json())
+      .then((game) => {
+        const scores = game.data.relationships.scores.map((score) => {
+          return fetch(`http://localhost:8000/game-scores/${score.id}`)
+            .then((res) => res.json());
+        });
+
+        return hash({
+          game: game.data,
+          scores: all(scores),
+        });
+      });
+  },
 });
